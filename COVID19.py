1e55f05b667f4fb83eb27591e572c82f7adff406
thurkj <73555271+thurkj@users.noreply.github.com>
2020-12-20 19:36:50 -0600

    Update COVID19.py

--- COVID19.py
+++ COVID19.py
@@ -1391,6 +1391,6 @@ app.layout = dbc.Container(fluid=True, children=[
 
 if __name__ == '__main__':
     #app.run_server(debug=True, use_reloader=False)  # Jupyter
-    app.run_server(debug=False)    # Use this line prior to heroku deployment
+    app.run_server(debug=True)    # Use this line prior to heroku deployment
     #application.run(debug=False, port=8080) # Use this line for AWS
 
